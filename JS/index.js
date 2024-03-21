152d0b01e7345d7b1429a597e070656ec3cc07e8
Tamara <tamy.sb@hotmail.com>
2024-03-21 17:33:50 -0300

    librerias

--- JS/index.js
+++ JS/index.js
@@ -6,11 +6,19 @@ class Producto{
     this.nombre = nombre
     this.cantidad = cantidad
     this.precio = precio
-    this.precioConIVA = function(){
-        let precioIVA = this.precio * 1.9;
-        return precioIVA
+    
+}   
+    IVA(){
+        this.IVA = this.precio*1.9
     }
-}
+    DESC(){
+        if(this.IVA >= 9000){
+            this.DESC = (this.IVA-(this.IVA * 0.20))
+        }
+        else{
+            this.DESC = 0
+        }
+    }
 }
 const productos=[
     new Producto(1,"producto1", 1, 5000),
@@ -19,12 +27,14 @@ const productos=[
     new Producto(4,"producto4", 1, 2000),
     new Producto(5,"producto5",1,3000)
 ]
-
+for(const producto of productos){
+    console.log(producto.IVA())
+    console.log(producto.DESC())
+}
 // Carro de Compras
 const contenedorProductos = document.getElementById('productos');
 const contenedorElementosCarrito = document.getElementById('elementos-carrito');
 const totalSpan = document.getElementById('total');
-const desc = document.getElementById('descuento')
 
 function renderizarProductos(){
     productos.forEach(producto =>{
@@ -32,8 +42,8 @@ function renderizarProductos(){
         div.classList.add('producto');
         div.innerHTML = `
         <h3>${producto.nombre}</h3>
-        <p>$${producto.precio}</p>
-        <p>$${producto.precioConIVA()} iva</p>
+        <p>precio:$${producto.precio}</p>
+        <p>precio:$${producto.IVA}(iva incluido)</p>
         <button class="btn-agregar-carrito" data-id="${producto.id}">comprar</button> 
         `;
         contenedorProductos.appendChild(div);
@@ -46,8 +56,11 @@ function renderizarProductos(){
 function agregarAlCarrito(idProducto){
     const elementosCarrito = JSON.parse(localStorage.getItem('carro'))||[];
     const itemExistente = elementosCarrito.find(item => item.id === idProducto);
+    
     if(itemExistente){
+        
         itemExistente.cantidad++
+        
     }else{
         const producto = productos.find(p => p.id === idProducto);
         if(producto){
@@ -77,7 +90,7 @@ function renderizarCarrito() {
     let precioTotal = 0;
     elementosCarrito.forEach(item =>{
             const li = document.createElement('li');
-            li.textContent = `${item.nombre} x ${item.cantidad}  - $${item.precio * item.cantidad}-$${(item.precio*1.9)*item.cantidad}`
+            li.textContent = `${item.nombre} x ${item.cantidad}  - $${item.precio * item.cantidad}-$${(item.IVA)*item.cantidad}-$${(item.DESC)*item.cantidad}`
             const btnEliminar = document.createElement('button');
             btnEliminar.textContent = 'eliminar';
             btnEliminar.addEventListener('click', () => eliminarDelCarrito(item.id))
@@ -91,18 +104,32 @@ function renderizarCarrito() {
 
 function realizarCompra(){
     const elementosCarrito = JSON.parse(localStorage.getItem('carro'))||[];
-    alert(`compra exitosa $${totalSpan.textContent}`);
+    Swal.fire({
+        position: "top-end",
+        icon: "success",
+        title: "compra exitosa",
+        text: `$${totalSpan.textContent}`,
+        showConfirmButton: true,
+        
+      })
     elementosCarrito.length = 0;
     localStorage.setItem('carro', JSON.stringify(elementosCarrito))
     renderizarCarrito();
 }
 
 document.getElementById('btn-comprar').addEventListener('click',realizarCompra);
+
 contenedorProductos.addEventListener('click',function(evento){
     if(evento.target.classList.contains('btn-agregar-carrito')){
             const idProducto = parseInt(evento.target.getAttribute('data-id'));
             agregarAlCarrito(idProducto);
         }
+        Toastify({
+            position: "right",
+            text: `$${totalSpan.textContent}`,
+            close: true,
+            
+          }).showToast()
     
 });
 
